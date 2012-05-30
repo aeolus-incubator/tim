5c797a111593ca02736400d14ab6140a8e5c5320
Martyn Taylor <mtaylor@redhat.com>
2012-05-30 18:28:32 +0100

    Added Templates API

--- app/controllers/image_management/templates_controller.rb
+++ app/controllers/image_management/templates_controller.rb
@@ -1,35 +1,34 @@
 module ImageManagement
   class TemplatesController < ApplicationController
     # GET /templates
-    # GET /templates.json
+    # GET /templates.xml
     def index
       @templates = ImageManagement::Template.all
 
       respond_to do |format|
         format.html # index.html.erb
-        format.json { render :json => @templates }
+        format.xml # index.xml
       end
     end
 
     # GET /templates/1
-    # GET /templates/1.json
+    # GET /templates/1.xml
     def show
       @template = ImageManagement::Template.find(params[:id])
 
       respond_to do |format|
         format.html # show.html.erb
-        format.json { render :json => @template }
+        format.xml # show.xml
       end
     end
 
     # GET /templates/new
-    # GET /templates/new.json
+    # GET /templates/new.xml
     def new
       @template = ImageManagement::Template.new
 
       respond_to do |format|
         format.html # new.html.erb
-        format.json { render :json => @template }
       end
     end
 
@@ -39,46 +38,61 @@ module ImageManagement
     end
 
     # POST /templates
-    # POST /templates.json
+    # POST /templates.xml
     def create
-      @template = ImageManagement::Template.new(params[:template])
-
       respond_to do |format|
-        if @template.save
-          format.html { redirect_to @template, :notice => 'Template was successfully created.' }
-          format.json { render :json => @template, :status => :created, :location => @template }
-        else
+        begin
+        # TODO Validate template
+          params[:template][:xml] = request.body.read
+          @template = ImageManagement::Template.new(params[:template])
+
+          if @template.save
+            format.html { redirect_to @template, :notice => 'Template was successfully created.' }
+            format.xml { render :action => "show", :status => :created }
+          else
+            format.html { render :action => "new" }
+            format.xml { render :xml => @template.errors, :status => :unprocessable_entity }
+          end
+          # TODO Add in proper exception handling in application controller
+        rescue => e
           format.html { render :action => "new" }
-          format.json { render :json => @template.errors, :status => :unprocessable_entity }
+          format.xml { render :xml => e.message, :status => :unprocessable_entity }
         end
       end
     end
 
     # PUT /templates/1
-    # PUT /templates/1.json
+    # PUT /templates/1.xml
     def update
-      @template = ImageManagement::Template.find(params[:id])
-
       respond_to do |format|
-        if @template.update_attributes(params[:template])
-          format.html { redirect_to @template, :notice => 'Template was successfully updated.' }
-          format.json { head :no_content }
-        else
-          format.html { render :action => "edit" }
-          format.json { render :json => @template.errors, :status => :unprocessable_entity }
+        begin
+          params[:template][:xml] = request.body.read
+          @template = ImageManagement::Template.find(params[:id])
+
+          if @template.update_attributes(params[:template])
+            format.html { redirect_to @template, :notice => 'Template was successfully updated.' }
+            format.xml { head :no_content }
+          else
+            format.html { render :action => "edit" }
+            format.xml { render :xml => @template.errors, :status => :unprocessable_entity }
+          end
+          # TODO Add in proper exception handling in appliation controller
+        rescue => e
+          format.html { render :action => "new" }
+          format.xml { render :xml => e.message, :status => :unprocessable_entity }
         end
       end
     end
 
     # DELETE /templates/1
-    # DELETE /templates/1.json
+    # DELETE /templates/1.xml
     def destroy
       @template = ImageManagement::Template.find(params[:id])
       @template.destroy
 
       respond_to do |format|
         format.html { redirect_to image_management_templates_url }
-        format.json { head :no_content }
+        format.xml { head :no_content }
       end
     end
   end
