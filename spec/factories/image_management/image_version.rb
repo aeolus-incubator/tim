0a6d01e99b0ec46773e13a2bb528401b1078db96
Martyn Taylor <mtaylor@redhat.com>
2012-06-28 12:08:20 +0100

    Updated Image Version API, tests and README
    Updated Create Image Version API
    Added Tests, README for show image version
    Updated spec, README for list images
    Updated spec, README for delete image version
    Updated Image Version Update API

--- spec/factories/image_management/image_version.rb
+++ spec/factories/image_management/image_version.rb
@@ -10,5 +10,13 @@ module ImageManagement
     factory :image_version_with_full_tree, :parent => :image_version do
       association :base_image, :factory => :base_image_with_template
     end
+
+    factory :image_version_with_target_images, :parent => :image_version_with_full_tree do
+      after_create do |image_version|
+        2.times do
+          image_version.target_images << Factory(:target_image, :image_version => image_version)
+        end
+      end
+    end
   end
 end
