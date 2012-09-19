407555fd5a3a70ee45b3b8a64dfb842cddc28fb5
Jason Guiditta <jguiditt@redhat.com>
2012-09-19 16:57:35 -0400

    More configuration updates.
    This adds some standard default settings that had not yet been ported
    into our 3.2 codebase, and some more setup for test.

--- lib/image_management/engine.rb
+++ lib/image_management/engine.rb
@@ -1,5 +1,8 @@
 module ImageManagement
   class Engine < Rails::Engine
     isolate_namespace ImageManagement
+    config.generators do |g|
+      g.test_framework :rspec, :view_specs => false
+    end
   end
 end
