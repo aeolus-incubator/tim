973be7d9113a3622f79569f906150dec0079b119
Jason Guiditta <jguiditt@redhat.com>
2012-09-24 16:23:46 -0400

    Update hooks for models.
    This patch moves a couple assumptions into the main code of the
    engine, as these non-core objects can reasonably be required to be
    provided by the host app. This has the net effect of less model
    class overrides needed by the host app.

--- test/dummy/app/models/image_management/target_image.rb
+++
@@ -1,7 +0,0 @@
-require File.expand_path('../../app/models/image_management/target_image', ImageManagement::Engine.called_from)
-
-module ImageManagement
-  class TargetImage
-    belongs_to :provider_type
-  end
-end
