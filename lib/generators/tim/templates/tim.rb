732b3d7d1fbab41bb057d99619446c82765743fe
Jason Guiditta <jguiditt@redhat.com>
2012-10-08 16:59:43 -0400

    Add initializer install generator.
    Resolves https://github.com/aeolus-incubator/tim/issues/40

---
+++ lib/generators/tim/templates/tim.rb
@@ -0,0 +1,8 @@
+Tim.user_class = "User"
+Tim.provider_account_class = "ProviderAccount"
+Tim.provider_type_class = "ProviderType"
+# Image Factory URL
+Tim::ImageFactory::Base.site = "http://localhost:8075/imagefactory"
+# FIXME: We should be able to infer these from Routes
+Tim::ImageFactory::TargetImage.callback_url = "http://localhost:3000/target_images/"
+Tim::ImageFactory::ProviderImage.callback_url = "http://localhost:3000/provider_images/"
