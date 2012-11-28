7249c59b04edbde6abb6781bd3080161ad842527
Martyn Taylor <mtaylor@redhat.com>
2012-11-28 11:21:37 +0000

    Catch XML Syntax Error exceptions in Template

--- app/validators/template_validator.rb
+++ app/validators/template_validator.rb
@@ -4,12 +4,16 @@ class TemplateValidator < ActiveModel::Validator
   @@template_rng = File.join(Tim::Engine.root, "config/schemas", "tdl.rng")
 
   def validate(record)
-    rng = Nokogiri::XML::RelaxNG(File.read(@@template_rng))
-    xml = Nokogiri::XML(record.xml) { |config| config.strict }
+    begin
+      rng = Nokogiri::XML::RelaxNG(File.read(@@template_rng))
+      xml = Nokogiri::XML(record.xml) { |config| config.strict }
 
-    rng.validate(xml).each do |error|
-      record.errors.add :xml, error.message
-    end
+      rng.validate(xml).each do |error|
+        record.errors.add :xml, error.message
+      end
+     rescue Nokogiri::XML::SyntaxError => e
+       record.errors.add :xml, "Syntax error on line #{e.line} at column #{e.column}: #{e.message}"
+     end
   end
 
 end
