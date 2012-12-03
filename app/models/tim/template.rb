7f9b2f3a2620e20fa78b81a7e8fe6d43d785dce0
Jan Provaznik <jprovazn@redhat.com>
2012-12-03 08:36:42 +0100

    Added os method to Template model

--- app/models/tim/template.rb
+++ app/models/tim/template.rb
@@ -10,10 +10,25 @@ module Tim
     attr_accessible :xml
     attr_protected :id
 
+    OS = Struct.new(:name, :version, :arch)
+
     # Used in views to display the xml elements of this template
     def xml_elements
-      ::Nokogiri::XML::Document.parse(xml).xpath("//template/*").to_xml
+      parsed_xml.xpath("//template/*").to_xml
     end
 
+    def os
+      OS.new(
+        parsed_xml.xpath("//template/os/name").text,
+        parsed_xml.xpath("//template/os/version").text,
+        parsed_xml.xpath("//template/os/arch").text
+      )
+    end
+
+    private
+
+    def parsed_xml
+      @parsed_xml ||= ::Nokogiri::XML::Document.parse(xml)
+    end
   end
 end
